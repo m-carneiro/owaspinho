892bb483af0ee42c8fe8704eac084c667a7a59fd
m-carneiro <m.carneiro.tech@gmail.com>
2025-07-21 10:30:41 -0300

    add path traversal and prototype pollution

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const _ = require('lodash');
 
 router.get('/search', function(req, res, next) {
   const query = req.query.q;
   res.render('search', { query: query });
+});
+
+router.get('/download', (req, res) => {
+  const file = req.query.file;
+  const filePath = path.join(__dirname, 'public', file);
+  res.download(filePath);
+});
+
+router.post('/merge', (req, res) => {
+  let obj = {};
+
+  _.merge(obj, req.body); // /merge com body: {"__proto__": {"isAdmin": true}}
+  res.send(obj);
 });
 
 module.exports = router;
